137fc7f41392f73da36b2850e836d090859b032e
jaya-12 <81163971+jaya-12@users.noreply.github.com>
2023-08-09 16:59:20 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,46 @@
+console.log("hello World!");
+//statement and comments
+//single line comment
+/*
+multiline comment
+*/
+// var a;
+// a = 9;
+// //alert("hello!");
+// console.log(a);
+// let b="hello!";
+// console.log(b);
+// const c=10;
+// console.log(c);
+//Js is a case sensitive language
+//operators
+let x=8;
+let y=5;
+let z;
+z=x+y;
+console.log(z);
+z=x-y;
+console.log(z);
+z=x*y;
+console.log(z);
+// z=x%y;
+// console.log(z);
+// //x=x+3
+// //x+=3;
+// console.log(z);
+z=y**2;
+console.log(z);
+let a=8;//number
+let b="8";//string 
+let c=8;
+console.log(a+b);
+console.log(a+c);
+a++;
+console.log(a);
+c--;
+console.log(c);
+y--;
+console.log(y);
+
+let p =(x+7)/7;
+console.log(p);
