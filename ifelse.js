3e78d14a90812214fdcfced6f0f084d06e6be44b
jaya-12 <81163971+jaya-12@users.noreply.github.com>
2023-08-10 18:27:44 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,28 @@
+// let age=19;
+// let hasVoterCard = "yes";
+// if(age>=18 && hasVoterCard == "yes")
+// {
+//     console.log("u can vote!");
+// }
+// if (age >= 18 && hasVoterCard == "No") {
+//   console.log("get your votercard!");
+// }
+// else {
+//   console.log("u cannot vote!");
+// }
+
+// let isloggedin =1;
+//  if(isloggedin==0){
+//     document.write('login');
+//  }else{
+//     document.write('logout');
+//  }
+
+//ternery operator
+// let isloggedin = 1;
+// let option = (isloggedin == 1)? "logout" :"login";
+//     document.write(option);
+
+let user;
+// user = "hjkl"
+alert(user ?? "guest");
