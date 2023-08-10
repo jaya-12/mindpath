678fa1580b2c9316c0796e0d261cce1650db4339
jaya-12 <81163971+jaya-12@users.noreply.github.com>
2023-08-10 10:24:07 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,58 @@
+let x = 'hel"lo'; //string
+console.log(x);
+let y = 'nin"ja'; //string
+console.log(y);
+let a = 1234; //number
+//boolean: true and false
+let b = 3 < 5;
+console.log(b);
+//array
+let i="jaduu";
+let l = ["tom", "jerry", "oggy", "jack"];
+//object
+let toy = { name: "tom", age: 50 };
+//undefined is also a data type
+//typeof
+let g = typeof "hhjjkk";
+let h = typeof 1234;
+
+console.log(g);
+console.log(h);
+let s = "Jaya";
+//alert(`hello ${i});//template literal // browser pe hoga
+
+
+
+//Comparison and logical operator
+//comparison opertor
+let m=2;
+let n=3;
+//==  -> equal to
+//5=='5' -> true
+//5==5->true
+//5==6->false
+
+
+// === -> equal value and equal type // data type chk krega phle fr value
+6 === " 6"
+//!= -> not equal to
+5!=7
+//!==  -> not equal value or not equal type
+5!==5
+// > -> greator than
+5>8
+// >= -> greater than or equal to
+8>=3
+//less than or equal to 
+4<=9
+
+
+//logical operators
+//&& and
+5<10 && 6>1
+
+// or
+5==5||6==5
+
+// ! not
+!(6==5)
