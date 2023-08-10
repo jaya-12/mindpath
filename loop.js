31e21c17be901082d47f992cf99e2fd18d31d284
jaya-12 <81163971+jaya-12@users.noreply.github.com>
2023-08-10 18:28:05 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,70 @@
+//while loop
+
+//print 1 to 10
+// let i=0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+//reverse print 1 to 10
+// let i=10;
+// while(i>=0)
+// {
+//     console.log(i);
+//     i--;
+// }
+
+// sum of all even numbers
+
+// let counter = 1;
+// let sum =0;
+// while(counter<=100){
+//     if(counter%2==0){
+//     sum =sum + counter;
+//     }
+//     counter++;
+// } 
+// console.log(sum);
+
+//do-while
+// let i=0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10)
+
+// for(let i=1;i<=10;i++){
+//     console.log("hi"+i);
+// }
+
+//break, continue and nested loop
+// for(let i=1;i<=10;i++){
+//     console.log("hi"+i);
+//     break;
+// }
+// for(let i=1;i<=10;i++){
+//     if (i==3){
+//         continue;
+//     }
+//     console.log("hi"+i);
+    
+// }
+
+//nested loop
+// let i,j;
+// for(i=0;i<4;i++){
+//     for(j=0;j<=i;j++){
+//         console.log("*");
+//     }
+//     console.log("\n");
+// }
+
+
+//foreach
+
+// let a=["apple","banana","cat"];
+// a.forEach(function(value,index){
+//     console.log(index+":" +value);
+// });
+
