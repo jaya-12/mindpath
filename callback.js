d46f90121478ec8bd5e5072f5f0c64ed2feb2e49
jaya-12 <81163971+jaya-12@users.noreply.github.com>
2023-08-14 14:52:23 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,55 @@
+//asynchronous
+
+//setTimeout
+// function myFirst(){
+//     myDisplayer("Hello");
+// }
+// function mySecond(){
+//     myDisplayer("goodbye");
+// }
+
+// function myDisplayer(value){
+//     console.log(value);
+// }
+
+// mySecond();
+// setTimeout(myFirst,5000);
+
+//--------------------
+
+//setInterval
+// program to display a text using setInterval method
+// function greet() {
+//   console.log("Hello world");
+// }
+
+// setInterval(greet, 1000);
+
+
+// function First(a, b, test) {
+//   const c = a + b;
+//   console.log("from First function");
+
+//   test(c);
+// }
+
+// function Second(x) {
+//   console.log("from second function", x);
+// }
+
+// First(2, 4, Second);
+
+//..........................
+
+// function k(kbr,mkt, guit ){
+//     let inst= kbr.concat("",mkt);
+//     guit(inst);
+// }
+// function y(n){
+//     console.log("musical instrument:",n);
+// }
+
+// kbr="gui";
+// mkt="tar";
+
+// k(kbr,mkt,y);
